9ad465aef8be6176496531cd59a895284c7190af
AceVentura7 <100761095+AceVentura7@users.noreply.github.com>
2022-04-16 08:15:40 +0000

    added title, url, thumbnail url, channel name, description. Views and uploaded are still incorrect. Need to fix

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import time
+
 
 yt_trending_url = 'https://www.youtube.com/feed/trending'
 
@@ -14,8 +16,10 @@ def get_driver():
   return driver
 
 def get_videos(driver):
+  
   video_div_tag = 'ytd-video-renderer'
   driver.get(yt_trending_url)
+  time.sleep(20)
   videos = driver.find_elements(By.TAG_NAME, video_div_tag)
   return videos
 
@@ -28,5 +32,30 @@ if __name__=="__main__":
   videos = get_videos(driver)
   
   print(f'Found {len(videos)} Videos')
-  
- 
+
+  print("Parsing the First Video")
+  #title, channel, views, uploaded, description, thumbnail_url, url
+
+video = videos[0]
+title_tag = video.find_element(By.ID,'video-title')
+title = title_tag.text
+url = title_tag.get_attribute('href')
+
+thumbnail_tag = video.find_element(By.TAG_NAME,'img')
+thumbnail_url = thumbnail_tag.get_attribute('src')
+
+description_tag = video.find_element(By.ID, 'description-text')
+desc = description_tag.text
+
+channel_div = video.find_element(By.CLASS_NAME, 'ytd-channel-name')
+channel_name = channel_div.text
+
+view_div = video.find_element(By. CLASS_NAME, 'ytd-video-meta-block')
+views = view_div.text
+
+print('Title:', title)
+print('URL:', url)
+print('Thumbnail URL:', thumbnail_url)
+print('Channel Name:', channel_name)
+print('Description:', desc)
+print('Views:', views)
