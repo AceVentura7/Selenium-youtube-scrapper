e77e5f354b1f75f85a1690880b850cd68d647cea
AceVentura7 <100761095+AceVentura7@users.noreply.github.com>
2022-04-16 10:46:45 +0000

    Extracted top 10 videos

--- scrapper.py
+++ scrapper.py
@@ -2,10 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
-
+import pandas as pd
 
 yt_trending_url = 'https://www.youtube.com/feed/trending'
-
 
 def get_driver():
   chrome_options = Options()
@@ -23,6 +22,29 @@ def get_videos(driver):
   videos = driver.find_elements(By.TAG_NAME, video_div_tag)
   return videos
 
+def parse_video(video):
+  title_tag = video.find_element(By.ID,'video-title')
+  title = title_tag.text
+  url = title_tag.get_attribute('href')
+  
+  thumbnail_tag = video.find_element(By.TAG_NAME,'img')
+  thumbnail_url = thumbnail_tag.get_attribute('src')
+  
+  description = video.find_element(By.ID, 'description-text').text
+  
+  channel_name = video.find_element(By.CLASS_NAME, 'ytd-channel-name').text
+  
+  view_div = video.find_element(By. CLASS_NAME, 'ytd-video-meta-block')
+  views = view_div.text
+
+  return{
+    'title': title,
+    'url': url,
+    'thumbnail_url': thumbnail_url,
+    'channel_name': channel_name,
+    'description': description,
+    'views': views
+  }
 
 if __name__=="__main__":
   print('Creating Driver')
@@ -33,29 +55,12 @@ if __name__=="__main__":
   
   print(f'Found {len(videos)} Videos')
 
-  print("Parsing the First Video")
+  print("Parsing Top 10 Video")
+  videos_data = [parse_video(video) for video in videos[:10]]
+
+  print('Save the data to a CSV')
+  videos_df = pd.DataFrame(videos_data)
+  print(videos_df)
+  videos_df.to_csv('tending.csv', index=None)
+  
   #title, channel, views, uploaded, description, thumbnail_url, url
-
-video = videos[0]
-title_tag = video.find_element(By.ID,'video-title')
-title = title_tag.text
-url = title_tag.get_attribute('href')
-
-thumbnail_tag = video.find_element(By.TAG_NAME,'img')
-thumbnail_url = thumbnail_tag.get_attribute('src')
-
-description_tag = video.find_element(By.ID, 'description-text')
-desc = description_tag.text
-
-channel_div = video.find_element(By.CLASS_NAME, 'ytd-channel-name')
-channel_name = channel_div.text
-
-view_div = video.find_element(By. CLASS_NAME, 'ytd-video-meta-block')
-views = view_div.text
-
-print('Title:', title)
-print('URL:', url)
-print('Thumbnail URL:', thumbnail_url)
-print('Channel Name:', channel_name)
-print('Description:', desc)
-print('Views:', views)
